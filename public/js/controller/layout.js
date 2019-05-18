96f9cdea327d0a939b62857d91557bf2d56b5a05
min163 <freetree97@naver.com>
2019-05-18 11:41:20 +0900

    190518_function_1

--- public/js/controller/layout.js
+++ public/js/controller/layout.js
@@ -3,3 +3,11 @@ Vue.filter('moment', function (date) {
     return moment(date).format('MM월 DD일');
 
 });
+
+Vue.filter('priority', function (str) {
+
+	if(str === "FIRST") return "매우 중요";
+	else if(str === "SECOND") return "중요";
+	else if(str === "THIRD") return "보통";
+	else return "중요하지 않음"
+});
